1da85ea2fe5b65ab22a2461c50d9f2982922b10a
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-01 17:26:04 -0600

    Day 20/21 project

---
+++ Day 21/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.write_score()
+
+    def write_score(self):
+        self.clear()
+        self.goto(0, 280)
+        message = f"Score: {self.score}"
+        self.write(message, align="center", font=("Arial", 14, "normal"))
+
+    def add_point(self):
+        self.score += 1
+        self.write_score()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=("Courier", 32, "bold"))
