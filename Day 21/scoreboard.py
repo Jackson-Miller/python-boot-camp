e1d35b9083d8ebd79d4c9b2e1557b1e24bef6d7d
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-04 17:53:38 -0600

    added high score abilities

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -13,13 +14,20 @@ class Scoreboard(Turtle):
     def write_score(self):
         self.clear()
         self.goto(0, 280)
-        message = f"Score: {self.score}"
+        message = f"Score: {self.score}    High Score: {self.high_score}"
         self.write(message, align="center", font=("Arial", 14, "normal"))
 
     def add_point(self):
         self.score += 1
         self.write_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font=("Courier", 32, "bold"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.write_score()
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align="center", font=("Courier", 32, "bold"))
