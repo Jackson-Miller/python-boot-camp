89d2927acd45ab42f349ddabb089177ea3c3b61d
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-04 18:11:19 -0600

    added high score abilities

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -5,7 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -24,9 +25,10 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.write_score()
-
 
     # def game_over(self):
     #     self.goto(0,0)
