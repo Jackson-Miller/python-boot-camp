a64e2b594a21c259e5194e5f71902ccb7916765b
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-10 19:07:28 -0600

    Day 30 was error handling, went back to update this project to use that.

--- Day 26/main.py
+++ Day 26/main.py
@@ -4,8 +4,17 @@ import pandas
 csv_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in csv_data.iterrows()}
 
-# Create a list with the phonetic name using list comprehension
-word = input("Enter a word: ").upper()
-phonetic_name = [nato_dict[letter] for letter in word]
 
-print(phonetic_name)
+# Create a list with the phonetic name using list comprehension
+def main():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_name = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter a word, numbers are not accepted.")
+        main()
+    else:
+        print(phonetic_name)
+
+
+main()
