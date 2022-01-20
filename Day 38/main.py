631e796979e567f2950db80a90d51e234a193769
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-19 20:44:15 -0600

    Day 38 Project

--- Day 38/main.py
+++ Day 38/main.py
@@ -5,7 +5,7 @@ from datetime import *
 NUTRITIONIX_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 NUTRITIONIX_APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
 NUTRITIONIX_API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
-SHEETY_ENDPOINT = "https://api.sheety.co/8191e27a182423e3999a53413bb663e5/workoutTracking/workouts"
+SHEETY_ENDPOINT = "https://api.sheety.co/"
 SHEETY_API_KEY = os.environ.get("SHEETY_API_KEY")
 
 nutritionix_auth_headers = {
@@ -37,12 +37,4 @@ for exercise in exercise_data:
         }
     }
 
-    response = requests.post(url=SHEETY_ENDPOINT, headers=sheety_auth_headers, json=workout_data)
-
-
-
-
-
-
-
-
+    requests.post(url=SHEETY_ENDPOINT, headers=sheety_auth_headers, json=workout_data)
