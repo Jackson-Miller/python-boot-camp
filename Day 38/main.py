a2e34d9c3903db27d71a6e2ca2c28d37468e49a7
Jackson Miller <Jackson-Miller@users.noreply.github.com>
2022-01-19 21:04:15 -0600

    Moved to using a .env file vs. local machine for environmental variables

--- Day 38/main.py
+++ Day 38/main.py
@@ -1,12 +1,14 @@
 import os
 import requests
 from datetime import *
+from dotenv import load_dotenv
 
+load_dotenv()
 NUTRITIONIX_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
-NUTRITIONIX_APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
-NUTRITIONIX_API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
+NUTRITIONIX_APP_ID = os.getenv("NUTRITIONIX_APP_ID")
+NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
 SHEETY_ENDPOINT = "https://api.sheety.co/8191e27a182423e3999a53413bb663e5/workoutTracking/workouts"
-SHEETY_API_KEY = os.environ.get("SHEETY_API_KEY")
+SHEETY_API_KEY = os.getenv("SHEETY_API_KEY")
 
 nutritionix_auth_headers = {
     "x-app-id": NUTRITIONIX_APP_ID,
